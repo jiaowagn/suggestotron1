2d55313ff88832b088daf17c5a775cf29fb4b9bc
jiaowagn <wangjiaohere@gmail.com>
2018-03-01 11:37:02 +0800

    topic and vote

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
+
+  get 'about' => "about#index"
+
+  get 'about2' => "topics#about"
+
+  root "topics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
